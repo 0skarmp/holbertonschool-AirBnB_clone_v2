9ea3c8132161ac492cd1110a69f76084892a848d
OSKAR <omoralespj@gmail.com>
2023-09-14 19:47:50 -0500

    script to start a web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,10 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/hbnb")
+@app.route("/hbnb" strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="5000", strict_slashes=False)
+    app.run(debug=True, host="0.0.0.0", port="5000")
