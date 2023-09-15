1928b0a64fb3e41288408980b9edb0349babca8c
0skarmp <omoralespj@gmail.com>
2023-09-14 17:37:39 -0700

    fixing bugs, spaces pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello HBNB"
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="5000")
