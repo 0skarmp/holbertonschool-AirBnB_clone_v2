feaaf0172c2225ea0d1e5b8c0caf2d114961cbf2
0skarmp <omoralespj@gmail.com>
2023-09-17 16:27:27 -0700

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""script to start a flask web"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+# route 1:
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+# route 2:
+@app.route("/hbnb", strict_slashes=False)
+def page2():
+    return "HBNB"
+
+
+# route 3:
+@app.route("/c/<text>", strict_slashes=False)
+def page3(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+# route 4:
+@app.route('/python/', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def page4(text="is_cool"):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+# route 5:
+@app.route("/number/<int:n>", strict_slashes=False)
+def page5(n):
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+# route 6
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def page6(n):
+    if isinstance(n, int):
+        return render_template("5-number.html", "n=n")
+
+
+# route 7
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def page7(n):
+    if isinstance(n, int):
+        num = f"{n} is odd"
+        if n % 2 == 0:
+            num = f"{n} is even"
+            return render_template("6-number_odd_or_even.html", num=num)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="5000")
