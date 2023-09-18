ce94cc16e5c050a3a9f1da450ddb9d8c732d21b0
0skarmp <omoralespj@gmail.com>
2023-09-17 22:40:33 -0700

    add method close

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,7 @@ class FileStorage:
         if obj is not None:
             key = f"{str(obj.__class__.__name__)}.{obj.id}"
             FileStorage.__objects.pop(key)
+
+    def close(self):
+        """method reload &  method for deserializing the JSON file to objects"""
+        self.reload()
