2e3935129ac31eb38a6e2a95520cc8ec36eb90ae
0skarmp <omoralespj@gmail.com>
2023-09-18 00:12:22 -0700

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,48 +1,41 @@
 #!/usr/bin/python3
-'''srcipt to start web flask'''
+""" Write a script that starts a Flask web application """
 
-from flask import Flask
+from flask import Flask, abort
+
 app = Flask(__name__)
 
 
-# route 1:
-@app.route("/", strict_slashes=False)
-def home():
-    """This function returns a greeting on the home page."""
+@app.route('/', strict_slashes=False)
+def h_route():
     return "Hello HBNB!"
 
 
-# route 2:
-@app.route("/hbnb", strict_slashes=False)
-def page2():
-    """This function returns 'HBNB'."""
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     return "HBNB"
 
 
-# route 3:
-@app.route("/c/<text>", strict_slashes=False)
-def page3(text):
-    """This function takes a 'text' parameter and displays it on the page."""
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
     text = text.replace("_", " ")
-    return f"C {text}"
+    return "C {}".format(text)
 
 
-# route 4:
 @app.route('/python/', strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def page4(text="is_cool"):
-    """function,text parameter & displays'Python{text}'on another the page."""
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
     text = text.replace("_", " ")
-    return f"Python {text}"
+    return "Python {}".format(text)
 
 
-# route 5:
-@app.route("/number/<int=n>", strict_slashes=False)
-def page5(n):
-    """function takes an integer'n'& displays if it is a number on the page."""
-    if isinstance(n, int):
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    if n.isdigit():
         return f"{n} is a number"
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
